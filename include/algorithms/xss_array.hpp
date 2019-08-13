a37f70ff3dec1ee6e627d546eb380d4dd97a916a
Jonas Ellert <jonas.ellert@tu-dortmund.de>
2019-08-13 15:02:14 +0200

    Add AL for array construction

--- include/algorithms/xss_array.hpp
+++ include/algorithms/xss_array.hpp
@@ -6,19 +6,17 @@
 
 class xss_array {
 private:
-  template <typename value_type,
-            typename index_type>
-  static void run_internal(const value_type* text,
-                           index_type* nss,
-                           const uint64_t n) {
+  template <typename value_type, typename index_type>
+  static void
+  run_internal(const value_type* text, index_type* nss, const uint64_t n) {
 
     index_type* pss = nss; // alias
 
-    auto print_array = [&] () {
-        for (index_type l = 0; l < n; ++l) {
-          std::cout << nss[l] << ", ";
-        }
-        std::cout << std::endl;
+    auto print_array = [&]() {
+      for (index_type l = 0; l < n; ++l) {
+        std::cout << nss[l] << ", ";
+      }
+      std::cout << std::endl;
     };
 
     std::stack<index_type> L;
@@ -36,7 +34,7 @@ private:
 
       while (text[j] > text[i]) {
         prev_j = j;
-        j = pss[j];
+        j = pss[prev_j];
         nss[prev_j] = i;
         L.pop();
       }
@@ -47,7 +45,7 @@ private:
 
       while (text[j + lce] > text[i + lce]) {
         prev_j = j;
-        j = pss[j];
+        j = pss[prev_j];
         nss[prev_j] = i;
         index_type ltop = L.top();
         L.pop();
@@ -63,7 +61,7 @@ private:
       pss[i] = j;
       L.push(lce);
 
-      if (unlikely(max_lce > 0)) {
+      if (unlikely(max_lce > 16)) {
         j = max_lce_j;
         const index_type distance = i - j;
         bool j_smaller_i = text[j + max_lce] < text[i + max_lce];
@@ -90,8 +88,7 @@ private:
             }
             pss[i] = last_pss;
           }
-          // FOR BOTH INCREASING AND DECREASING RUNS (fill areas between start
-          // positions)
+          // FOR BOTH RUN DIRECTIONS (fill areas between start positions)
           for (index_type k = j + distance; k < i;) {
             const index_type next_limit = k + distance;
             for (++k; k < next_limit; ++k) {
@@ -100,83 +97,79 @@ private:
           }
         }
         // AMORTIZED LOOK-AHEAD
-//        else {
-//          const index_type ell = div<4>(max_lce);
-//          index_type anchor = ell;
-//
-//          // check if gamm_ell is an extended lyndon run
-//          const auto lce_str = &(text[i]);
-//          const auto duval =
-//              is_extended_lyndon_run(&(lce_str[ell]), max_lce - ell);
-//
-//          // try to extend the lyndon run as far as possible to the left
-//          if (duval.first > 0) {
-//            const index_type period = duval.first;
-//            const auto repetition_eq = [&](const index_type l,
-//                                           const index_type r) {
-//              for (index_type k = 0; k < period; ++k)
-//                if (unlikely(lce_str[l + k] != lce_str[r + k]))
-//                  return false;
-//              return true;
-//            };
-//            int64_t lhs = ell + duval.second - period;
-//            while (lhs >= 0 && repetition_eq(lhs, lhs + period)) {
-//              lhs -= period;
-//            }
-//            anchor = std::min(ell, (index_type)(lhs + 2 * period));
-//          }
-//
-//          //           if(anchor < ell)
-////          std::cout << "AL " << j << " " << i << " " << max_lce << " " << ell
-////                    << " " << anchor << std::endl;
-//
-//          for (index_type k = 1; k < anchor; ++k) {
-//            if constexpr (nss_enabled)
-//              if (nss[j + k] < j + anchor) {
-//                nss[i + k] = nss[j + k] + distance;
-////                std::cout << i + k << " " << nss[i + k] << " " << nss[j + k]
-////                          << std::endl;
-//              }
-//            if constexpr (pss_enabled)
-//              pss[i + k] = pss[j + k] + distance;
-//          }
-//
-//          if constexpr (nss_enabled) {
-//            for (index_type k = 1; k < anchor; ++k) {
-//              if (nss[i + k] == 0) {
-//                L.push(get_lce(H.top(), i + k));
-//                H.push(i + k);
-//              }
-//            }
-//          } else {
-//            for (index_type k = 1; k < anchor; ++k) {
-//              while (H.top() != pss[i + k]) {
-//                H.pop();
-//                L.pop();
-//              }
-//              L.push(get_lce(H.top(), i + k));
-//              H.push(i + k);
-//            }
-//          }
-//
-//          i += anchor - 1;
-//        }
+        else {
+          const index_type ell = div<4>(max_lce);
+          index_type anchor = ell;
+
+          // check if gamm_ell is an extended lyndon run
+          const auto lce_str = &(text[i]);
+          const auto duval =
+              is_extended_lyndon_run(&(lce_str[ell]), max_lce - ell);
+
+          // try to extend the lyndon run as far as possible to the left
+          if (duval.first > 0) {
+            const index_type period = duval.first;
+            const auto repetition_eq = [&](const index_type l,
+                                           const index_type r) {
+              for (index_type k = 0; k < period; ++k)
+                if (unlikely(lce_str[l + k] != lce_str[r + k]))
+                  return false;
+              return true;
+            };
+            int64_t lhs = ell + duval.second - period;
+            while (lhs >= 0 && repetition_eq(lhs, lhs + period)) {
+              lhs -= period;
+            }
+            anchor = std::min(ell, (index_type)(lhs + mul<2>(period)));
+          }
+
+          // copy NSS values up to anchor
+          for (index_type k = 1; k < anchor; ++k) {
+            if (nss[j + k] < j + anchor) {
+              nss[i + k] = nss[j + k] + distance;
+            }
+          }
+
+          // update PSS links and LCP values
+          index_type htop = i;
+          for (index_type k = 1; k < anchor; ++k) {
+            if (nss[i + k] == 0) {
+              index_type next_lce = get_lce(htop, i + k);
+              L.push(next_lce);
+              pss[i + k] = htop;
+              htop = i + k;
+
+              const index_type distance = htop - pss[htop];
+              while (next_lce >= mul<2>(distance)) {
+                if (k + distance >= anchor)
+                  break;
+                if (nss[htop + distance] != 0)
+                  break;
+                k += distance;
+                next_lce -= distance;
+                L.push(next_lce);
+                pss[i + k] = htop;
+                htop = i + k;
+              }
+            }
+          }
+
+          i += anchor - 1;
+        }
       }
     }
 
     j = n - 2;
     while (j > 0) {
       prev_j = j;
-      j = pss[j];
+      j = pss[prev_j];
       nss[prev_j] = n - 1;
     }
   }
 
 public:
   template <typename value_type, typename index_type>
-  static void run(const value_type* text,
-                  index_type* nss,
-                  const uint64_t n) {
-      run_internal(text, nss, n);
+  static void run(const value_type* text, index_type* nss, const uint64_t n) {
+    run_internal(text, nss, n);
   }
 };
